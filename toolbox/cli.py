bcbd1155f1bec184e83cd9efd362db0ce90c560c
Song Jin <songjin@hotmail.com>
2017-08-22 21:16:17 +1000

    commit left overs

--- toolbox/cli.py
+++ toolbox/cli.py
@@ -7,13 +7,15 @@ def parse_args():
                         '--user',
                         dest='user',
                         help='github username')
+
     parser.add_argument('-t',
                         '--token',
                         dest='token',
                         help='personal access or OAuth token, or path to token (file://...)')
+
     parser.add_argument('-O',
                         '--organization',
-                        # action='store_true',
                         dest='organization',
                         help='whether or not this is an organization user')
+
     return parser.parse_known_args()
